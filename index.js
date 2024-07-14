3b8b5ccce5325d1a23b24c594d2e6a0307b3f896
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-07-14 19:57:39 +0600

    Server setpu and mongodb connected and created one get api

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 9000;
+
+const app = express();
+
+const corsOption = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  Credential: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOption));
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s3zryfx.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    const jobsCollection = client.db("solosphere").collection("jobs");
+    const bidsCollection = client.db("solosphere").collection("bids");
+
+    // get the all jobs data from db
+    app.get("/jobs", async (req, res) => {
+      const result = await jobsCollection.find().toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello from SoloSphere Server......");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
